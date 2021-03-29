e4edb593f7e920ff50fafec9336cf1ddb9eed82d
João Alves <joao.alves@liberali.local>
2021-03-29 08:53:05 -0400

    Implementação da estrutura de interface comprable

---
+++ Entites/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace IComparableInterface.Entites
+{
+    class Employee : IComparable
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string csvEmployee)
+        {
+            string[] vect = csvEmployee.Split(',');
+            Name = vect[0];
+            Salary = double.Parse(vect[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return Name + "," + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Employee))
+            {
+                throw new ArgumentException("Comparing error: argument is not type employ");
+            }
+            Employee other = obj as Employee;
+            return Name.CompareTo(other.Name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using IComparableInterface.Entites;
+
+namespace IComparableInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\joao.alves\source\repos\IComparableInterface\comparable.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while(!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach(Employee str in list)
+                    {
+                        Console.WriteLine(str);
+                    }
+                }
+            } catch(IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
